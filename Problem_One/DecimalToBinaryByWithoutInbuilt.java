c829e46c4939f07e0387e33ed0cce1521c2a5e85
Naman Kumar Singh <singhnaman320@gmail.com>
2023-09-07 09:02:49 +0530

    Problem_1: Decimal to binary without inbuilt function

--- Problem_One/DecimalToBinaryByWithoutInbuilt.java
+++ Problem_One/DecimalToBinaryByWithoutInbuilt.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class DecimalToBinaryByWithoutInbuilt {
+
+    public static String convert(String inputString){
+
+        int decimalValue = Integer.parseInt(inputString);
+        StringBuilder binaryValue = new StringBuilder();
+
+        if (decimalValue <= 0){
+
+            binaryValue.append("0");
+
+        }else{
+
+            while (decimalValue > 0){
+
+                int outcome = decimalValue % 2;
+                // Adding outcome as remainder in beginning
+                binaryValue.insert(0, outcome);
+                decimalValue = decimalValue / 2;
+            }
+        }
+
+        return binaryValue.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        String decimalString = scanner.next();
+
+        System.out.println(convert(decimalString));
+    }
 }
