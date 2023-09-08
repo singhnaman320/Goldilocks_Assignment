367d57263f104764f6235eca8abc9cfd9d1d46a0
Naman Kumar Singh <singhnaman320@gmail.com>
2023-09-08 11:12:54 +0530

    Problem-4: Call HTTP URL

---
+++ Problem_Four/ProblemFour/src/test/java/com/goldilocks/ProblemFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldilocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
