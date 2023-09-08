03c3ce8e940c2b74ddf4fcd08c2116689caa8f87
Naman Kumar Singh <singhnaman320@gmail.com>
2023-09-08 16:18:41 +0530

    Problem-4: Call HTTP URL

--- Problem_Four/ProblemFour/src/main/java/com/goldilocks/controllers/EmployeeController.java
+++ Problem_Four/ProblemFour/src/main/java/com/goldilocks/controllers/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goldilocks.exceptions.EmployeeNotFoundException;
@@ -31,15 +32,17 @@ public class EmployeeController {
 		return new ResponseEntity<Employee>(savEmployee, HttpStatus.CREATED);
 	}
 	
-	@GetMapping(value = "/test/{Id}")
-	public ResponseEntity<Employee> getEmployeeByIdHandler(@PathVariable("Id") Integer empId) throws EmployeeNotFoundException{
+	// problem statement valid here
+	
+	@GetMapping(value = "/test")
+	public ResponseEntity<Employee> getEmployeeByIdHandler(@RequestParam("value") Integer empId) throws EmployeeNotFoundException{
 		
 		Employee employee= eService.getEmployeeById(empId);
 		
 		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
 	}
 	
-	@GetMapping(value = "/test")
+	@GetMapping(value = "/testAll")
 	public ResponseEntity<List<Employee>> getAllEmployeeDetailsHandler() throws EmployeeNotFoundException{
 		
 		List<Employee> employeesList= eService.getAllEmployeeDetails();
