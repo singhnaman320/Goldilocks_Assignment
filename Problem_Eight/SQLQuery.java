629419d1494a1a9b9fd88c68df62f8588da55a29
Naman Kumar Singh <singhnaman320@gmail.com>
2023-09-09 11:44:31 +0530

    Problem-8: MYSQL Query

---
+++ Problem_Eight/SQLQuery.java
@@ -0,0 +1,10 @@
+
+/*
+    ======================== MYSQL Query =========================
+
+    SELECT name, age FROM people WHERE age < 30 AND name LIKE 'S%';
+
+    ===============================================================
+*/
+public class SQLQuery {
+}
